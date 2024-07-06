0b05fdf3cd0e496629ce3a8f26032aff1e33b55a
Yeonsuk Yoo <yysuk0302@gmail.com>
2024-07-07 02:48:46 +1000

    6.6 Encapsulate Variable

--- ch6/6-6-use.js
+++ ch6/6-6-use.js
@@ -1,6 +1,6 @@
 import { getDefaultOwner } from './6-6.js';
 
 const owner = getDefaultOwner();
-owner.firstName = '엘리';
+owner.firstName = '엘리'; // Error!
 console.log(owner);
 console.log(getDefaultOwner());
