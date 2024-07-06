03e3498245570f2aa70555e4dede040a3168d73b
Yeonsuk Yoo <yysuk0302@gmail.com>
2024-07-07 02:46:04 +1000

    6.1 Extract Function

--- ch6/6-1.js
+++ ch6/6-1.js
@@ -1,24 +1,30 @@
 export function printOwing(invoice) {
-  let outstanding = 0;
+  printBanner();
+  const outstanding = calculateOutstanding(invoice);
+  recordDueDate(invoice);
+  printInvoiceDetails(invoice, outstanding);
+}
 
+function printBanner() {
   console.log('***********************');
-  console.log('**** Customer Owes ****');
+  console.log(`**** Customer Owes ****`);
   console.log('***********************');
+}
 
-  // calculate outstanding
-  for (const o of invoice.orders) {
-    outstanding += o.amount;
-  }
+function calculateOutstanding(invoice) {
+  return invoice.orders.reduce((sum, order) => (sum += order.amount), 0);
+}
 
-  // record due date
+function recordDueDate(invoice) {
   const today = new Date();
   invoice.dueDate = new Date(
     today.getFullYear(),
     today.getMonth(),
     today.getDate() + 30
   );
+}
 
-  //print details
+function printInvoiceDetails(invoice, outstanding) {
   console.log(`name: ${invoice.customer}`);
   console.log(`amount: ${outstanding}`);
   console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
