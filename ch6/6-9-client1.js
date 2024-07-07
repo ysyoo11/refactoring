761a2d910297ff5d81d7baa1032648530c1c6432
Yeonsuk Yoo <yysuk0302@gmail.com>
2024-07-07 11:44:11 +1000

    6.9 Combine Functions into Class

--- ch6/6-9-client1.js
+++ ch6/6-9-client1.js
@@ -1,6 +1,4 @@
-import { acquireReading, baseRate } from './6-9.js';
+import { acquireReading } from './6-9.js';
 
-const aReading = acquireReading();
-
-const baseCharge = baseRate(aReading.month, aReading.year) * aReading.quantity;
-console.log(baseCharge);
+const reading = acquireReading();
+console.log(reading.baseCharge);
--- ch6/6-9-client3.js
+++ ch6/6-9-client3.js
@@ -1,9 +1,4 @@
-import { acquireReading, baseRate } from './6-9.js';
+import { acquireReading } from './6-9.js';
 
-const aReading = acquireReading();
-
-function calculateBaseCharge(aReading) {
-  return baseRate(aReading.month, aReading.year) * aReading.quantity;
-}
-
-const basicChargeAmount = calculateBaseCharge(aReading);
+const reading = acquireReading();
+const basicChargeAmount = reading.baseCharge;
