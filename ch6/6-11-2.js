49c6cd5f8f42fb1cf32c3bb9af749dc9bb081513
Yeonsuk Yoo <yysuk0302@gmail.com>
2024-07-07 14:19:21 +1000

    6.11 Split Phase

--- ch6/6-11-2.js
+++ ch6/6-11-2.js
@@ -1,18 +1,36 @@
 import fs from 'fs';
 
-if (!process.argv[2]) {
-  throw new Error('파일 이름을 입력하세요');
+function run(args) {
+  const command = parseCommand(args);
+  countOrders(command);
 }
 
-const fileName = `./${process.argv[2]}.json`;
-if (!fs.existsSync(fileName)) {
-  throw new Error('파일이 존재하지 않습니다');
+run(process.argv);
+
+function parseCommand(args) {
+  if (!process.argv[2]) {
+    throw new Error('파일 이름을 입력하세요');
+  }
+
+  const fileName = `./${args[2]}.json`;
+  if (!fs.existsSync(fileName)) {
+    throw new Error('파일이 존재하지 않습니다');
+  }
+
+  const countReadyOnly = process.argv.includes('-r');
+
+  return {
+    fileName,
+    countReadyOnly,
+  };
 }
 
-const rawData = fs.readFileSync(fileName);
-const orders = JSON.parse(rawData);
-if (process.argv.includes('-r')) {
-  console.log(orders.filter((order) => order.status === 'ready').length);
-} else {
-  console.log(orders.length);
+function countOrders({ fileName, countReadyOnly }) {
+  const rawData = fs.readFileSync(fileName);
+  const orders = JSON.parse(rawData);
+  const filtered = countReadyOnly
+    ? orders.filter((order) => order.status === 'ready')
+    : orders;
+
+  console.log(filtered.length);
 }
--- ch6/6-11.js
+++ ch6/6-11.js
@@ -1,16 +1,22 @@
 export function priceOrder(product, quantity, shippingMethod) {
   const basePrice = product.basePrice * quantity;
-  const discount =
+  const discount = calculateDiscount(product, quantity);
+  const shippingCost = calculateShipping(basePrice, shippingMethod, quantity);
+  return basePrice - discount + shippingCost;
+}
+function calculateDiscount(product, quantity) {
+  return (
     Math.max(quantity - product.discountThreshold, 0) *
     product.basePrice *
-    product.discountRate;
+    product.discountRate
+  );
+}
+function calculateShipping(basePrice, shippingMethod, quantity) {
   const shippingPerCase =
     basePrice > shippingMethod.discountThreshold
       ? shippingMethod.discountedFee
       : shippingMethod.feePerCase;
-  const shippingCost = quantity * shippingPerCase;
-  const price = basePrice - discount + shippingCost;
-  return price;
+  return quantity * shippingPerCase;
 }
 
 // 사용 예:
