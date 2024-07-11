b7bcfc0b5ba05c9cff6ccb69f6d1c6d2d8331b10
Yeonsuk Yoo <yysuk0302@gmail.com>
2024-07-11 20:53:24 +1000

    9.5 Change Value to Reference

--- ch9/9-5.js
+++ ch9/9-5.js
@@ -1,7 +1,19 @@
+const customerRepository = new CustomerRepository();
+
+const data = {
+  number: '0000001',
+  customerId: 'ysyoo11',
+};
+
+const order = new Order(
+  data.number,
+  customerRepository.registerCustomer(data.customerId)
+);
+
 class Order {
-  constructor(data) {
-    this._number = data.number;
-    this._customer = new Customer(data.customerId);
+  constructor(number, customer) {
+    this._number = number;
+    this._customer = customer;
   }
 
   get customer() {
@@ -16,5 +28,21 @@ class Customer {
 
   get id() {
     return this._id;
+  }
+}
+
+class CustomerRepository {
+  #customers;
+  constructor() {
+    this.#customers = new Map();
+  }
+  registerCustomer(id) {
+    if (!this.#customers.has(id)) {
+      this.#customers.set(id, new Customer(id));
+    }
+    return findCustomer(id);
+  }
+  findCustomer(id) {
+    return this.#customers.get(id);
   }
 }
