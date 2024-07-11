b00de1eab69872ae350840d0fa60345835adf222
Yeonsuk Yoo <yysuk0302@gmail.com>
2024-07-11 17:07:20 +1000

    8.4 Move Statements to Callers

--- ch8/8-4.js
+++ ch8/8-4.js
@@ -2,6 +2,7 @@ function renderPerson(outStream, person) {
   outStream.write(`<p>${person.name}</p>\n`);
   renderPhoto(outStream, person.photo);
   emitPhotoData(outStream, person.photo);
+  outStream.write(`<p>location: ${person.photo.location}</p>\n`);
 }
 
 function listRecentPhotos(outStream, photos) {
@@ -10,6 +11,7 @@ function listRecentPhotos(outStream, photos) {
     .forEach((p) => {
       outStream.write('<div>\n');
       emitPhotoData(outStream, p);
+      outStream.write(`<p>location: ${p.location}</p>\n`);
       outStream.write('</div>\n');
     });
 }
@@ -17,10 +19,9 @@ function listRecentPhotos(outStream, photos) {
 function emitPhotoData(outStream, photo) {
   outStream.write(`<p>title: ${photo.title}</p>\n`);
   outStream.write(`<p>date: ${photo.date.toDateString()}</p>\n`);
-  outStream.write(`<p>location: ${photo.location}</p>\n`);
 }
 
-function renderPhoto(outStream, aPhoto) {
+function renderPhoto(outStream, photo) {
   outStream.write('');
 }
 
