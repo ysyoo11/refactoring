db5c70134e01e44a269ccac1b69877d47ea3e8a1
Yeonsuk Yoo <yysuk0302@gmail.com>
2024-07-13 01:32:03 +1000

    10.6 Introduce Assertion

--- ch10/10-6.js
+++ ch10/10-6.js
@@ -1,8 +1,17 @@
+import { strict as assert } from 'node:assert';
+
 class Customer {
   constructor() {
     this.discountRate = 0;
   }
   applyDiscount(number) {
+    assert(number >= 0);
     return this.discountRate ? number - this.discountRate * number : number;
   }
 }
+
+new Customer().applyDiscount(-1);
+
+// 이렇게 assert를 쓰기 보다는 다른 방식으로 우아하게 처리하는게 더 좋음
+// 예를 들면, assert를 한 번 감싸서 클라이언트에서 에러가 발생했을 경우 앱을 죽게하는게 아니라 bug report를 보내게 만든다던지,
+// number를 디폴트 0으로 만들어준다던지 등
