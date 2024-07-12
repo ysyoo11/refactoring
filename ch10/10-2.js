0fc7c0b2e42db1a3305c6e6acbb315c2c18ed624
Yeonsuk Yoo <yysuk0302@gmail.com>
2024-07-12 12:46:54 +1000

    10.2 Consolidate Conditional Expression

--- ch10/10-2.js
+++ ch10/10-2.js
@@ -1,6 +1,11 @@
 function disabilityAmount(employee) {
-  if (employee.seniority < 2) return 0;
-  if (employee.monthsDisabled > 12) return 0;
-  if (employee.isPartTime) return 0;
-  return 1;
+  return isNotEligibleForDisability(employee) ? 0 : 1;
+}
+
+function isNotEligibleForDisability(employee) {
+  return (
+    employee.seniority < 2 ||
+    employee.monthsDisabled > 12 ||
+    employee.isPartTime
+  );
 }
