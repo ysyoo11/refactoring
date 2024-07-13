0d32358fbd5a78247bda8fdff23f97f3db6df1bd
Yeonsuk Yoo <yysuk0302@gmail.com>
2024-07-13 23:50:56 +1000

    11.9 Replace Function with Command

--- ch11/11-9.js
+++ ch11/11-9.js
@@ -1,20 +1,47 @@
-export function score(candidate, medicalExam, scoringGuide) {
-  let result = 0;
-  let healthLevel = 0;
-  let highMedicalRiskFlag = false;
+// Replace Function with Command
 
-  if (medicalExam.isSmoker) {
-    healthLevel += 10;
-    highMedicalRiskFlag = true;
+/**
+ * Command Pattern:
+ * 클래스 중에서 딱 한가지 일만 수행하는 함수를 갖고있는 클래스를 명령 객체라고 한다.
+ * 이런 패턴을 command pattern 이라고 한다
+ * 이 패턴을 사용한다고 해서 딱 하나의 함수만 있어야 하는 것은 아니다
+ * 만약 do라는 함수가 있다면, undo 함수도 만들어서 실행한 것을 취소하도록 만들 수도 있을 것이다
+ */
+
+export function score(candidate, medicalExam, scoringGuide) {
+  return new Scorer(candidate, medicalExam, scoringGuide).execute();
+}
+
+class Scorer {
+  #candidate;
+  #medicalExam;
+  #scoringGuide;
+  constructor(candidate, medicalExam, scoringGuide) {
+    this.#candidate = candidate;
+    this.#medicalExam = medicalExam;
+    this.#scoringGuide = scoringGuide;
   }
-  let certificationGrade = 'regular';
-  if (scoringGuide.stateWithLowCertification(candidate.originState)) {
-    certificationGrade = 'low';
-    result -= 5;
+
+  execute() {
+    let result = 0;
+    let healthLevel = 0;
+    let highMedicalRiskFlag = false;
+
+    if (this.#medicalExam.isSmoker) {
+      healthLevel += 10;
+      highMedicalRiskFlag = true;
+    }
+    let certificationGrade = 'regular';
+    if (
+      this.#scoringGuide.stateWithLowCertification(this.#candidate.originState)
+    ) {
+      certificationGrade = 'low';
+      result -= 5;
+    }
+    // lots more code like this
+    result -= Math.max(healthLevel - 5, 0);
+    return result;
   }
-  // lots more code like this
-  result -= Math.max(healthLevel - 5, 0);
-  return result;
 }
 
 export class ScoringGuide {
