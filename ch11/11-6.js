56b667f229c1a69889ee10b92ee6927339c9df2d
Yeonsuk Yoo <yysuk0302@gmail.com>
2024-07-13 23:19:13 +1000

    11.6 Replace Query with Parameter

--- ch11/11-6.js
+++ ch11/11-6.js
@@ -1,7 +1,8 @@
-targetTemperature(aPlan);
+// Replace Query with Parameter
+
+targetTemperature(aPlan, thermostat.currentTemperature);
 
 // 다른모듈에 있는 함수라고 가정
-function targetTemperature(aPlan) {
-  currentTemperature = thermostat.currentTemperature;
+function targetTemperature(aPlan, currentTemperature) {
   // ...
 }
