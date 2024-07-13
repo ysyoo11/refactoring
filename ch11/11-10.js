ea507091db4014d79656b5afb725fc9e1c2a793d
Yeonsuk Yoo <yysuk0302@gmail.com>
2024-07-13 23:55:16 +1000

    11.10 Replace Command with Function

--- ch11/11-10.js
+++ ch11/11-10.js
@@ -1,13 +1,12 @@
-export class ChargeCalculator {
-  constructor(customer, usage, provider) {
-    this._customer = customer;
-    this._usage = usage;
-    this._provider = provider;
-  }
-  get baseCharge() {
-    return this._customer.baseRate * this._usage;
-  }
-  get charge() {
-    return this.baseCharge + this._provider.connectionCharge;
-  }
+// Replace Command with Function
+
+/**
+ * 데이터를 영원히 간직할 필요가 없다면,
+ * 일시적으로 한 번만 연산하는거라면 그냥 일반 함수로 만들어 두는 것이 더 낫다
+ * 리팩토링 시에 100% 정해진 정답은 없기 때문에 목적에 맞는 최선의 선택을 하면 된다
+ */
+
+function charge(customer, usage, provider) {
+  const baseCharge = customer.baseRate * usage;
+  return baseCharge + provider.connectionCharge;
 }
