652a20b3144904a8b1162d68f13aeb58d165b858
Yeonsuk Yoo <yysuk0302@gmail.com>
2024-07-13 22:57:15 +1000

    11.2 Parameterize Function

--- ch11/11-2.js
+++ ch11/11-2.js
@@ -1,30 +1,20 @@
 // 예제 1
-function tenPercentRaise(person) {
-  person.salary = person.salary.multiply(1.1);
-}
-
-function fivePercentRaise(person) {
-  person.salary = person.salary.multiply(1.05);
+function raise(person, factor) {
+  person.salary = person.salary.multiply(1 + factor);
 }
 
 // 예제 2
 export function baseCharge(usage) {
   if (usage < 0) return usd(0);
   const amount =
-    bottomBand(usage) * 0.03 + middleBand(usage) * 0.05 + topBand(usage) * 0.07;
+    withinBand(usage, 0, 100) * 0.03 +
+    withinBand(usage, 100, 200) * 0.05 +
+    withinBand(usage, 200) * 0.07;
   return usd(amount);
 }
 
-function bottomBand(usage) {
-  return Math.min(usage, 100);
-}
-
-function middleBand(usage) {
-  return usage > 100 ? Math.min(usage, 200) - 100 : 0;
-}
-
-function topBand(usage) {
-  return usage > 200 ? usage - 200 : 0;
+function withinBand(usage, bottom, top = Infinity) {
+  return usage > bottom ? Math.min(usage, top) - bottom : 0;
 }
 
 function usd(value) {
@@ -34,3 +24,5 @@ function usd(value) {
     value: value,
   };
 }
+
+console.log(baseCharge(150));
