f3f6554f674c6d971b6b9845843dc1f29cf7cc9c
Yeonsuk Yoo <yysuk0302@gmail.com>
2024-07-14 00:06:53 +1000

    11.12 Replace Error Code with Exception

--- ch11/11-12.js
+++ ch11/11-12.js
@@ -1,4 +1,20 @@
 function localShippingRules(data) {
-  if (data) return new ShippingRules(data);
-  else return -23;
+  if (!data) throw new OrderProcessingError(-23);
+
+  return new ShippingRules(data);
+}
+
+class OrderProcessingError extends Error {
+  constructor(errorCode) {
+    super();
+    this.errorCode = errorCode;
+  }
+}
+
+try {
+  const result = localShippingRules();
+} catch (error) {
+  if (error instanceof OrderProcessingError) {
+    console.log(error);
+  }
 }
