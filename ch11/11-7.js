c4ae2f615bf24421215e6463fe42a5aa7d6137b9
Yeonsuk Yoo <yysuk0302@gmail.com>
2024-07-13 23:31:37 +1000

    11.7 Remove Setting Method

--- ch11/11-7.js
+++ ch11/11-7.js
@@ -1,4 +1,10 @@
+// Removing Setting Method (setter)
+
 class Person {
   get name() {}
-  set name(value) {}
 }
+
+/**
+ * 읽기만 가능한거라면 setter 는 불필요하니까 지우기
+ * 데이터를 변경할 수 있지만 특정한 유효성 검사가 필요하다면 setter를 통해서 유효성 검사 로직을 추가해서 넣자
+ */
