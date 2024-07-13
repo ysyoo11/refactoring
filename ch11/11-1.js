0051e15905685b28b6d9256ea661c32dbc5459a5
Yeonsuk Yoo <yysuk0302@gmail.com>
2024-07-13 22:36:44 +1000

    11.1 Separate Query from Modifier

--- ch11/11-1.js
+++ ch11/11-1.js
@@ -1,23 +1,24 @@
 // 예제 1
-function totalOutstandingAndSendBill() {
-  const result = customer.invoices.reduce(
-    (total, each) => each.amount + total,
-    0
-  );
-  sendBill();
-  return result;
+function getTotalOutstanding() {
+  return customer.invoices.reduce((total, each) => each.amount + total, 0);
+}
+
+function sendBill() {
+  // send bill
 }
 
 // 예제 2
 export function alertForMiscreant(people, alarm) {
+  const miscreant = findMiscreant(people);
+  setOffAlarms(alarm, miscreant);
+}
+
+function findMiscreant(people) {
+  const miscreants = ['Don', 'John'];
   for (const p of people) {
-    if (p === 'Don') {
+    if (miscreants.includes(p)) {
       setOffAlarms(alarm, p);
-      return 'Don';
-    }
-    if (p === 'John') {
-      setOffAlarms(alarm, p);
-      return 'John';
+      return p;
     }
   }
   return '';
