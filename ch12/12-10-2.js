71692731236098ee2fe2f7291011a7d5f8d15d3c
Yeonsuk Yoo <yysuk0302@gmail.com>
2024-07-16 21:01:42 +1000

    12.10 Replace Subclass with Delegate - Author's examples

--- ch12/12-10-2.js
+++ ch12/12-10-2.js
@@ -1,20 +1,12 @@
-function createBird(bird) {
-  switch (bird.type) {
-    case '유럽 제비':
-      return new EuropeanSwallow(bird);
-    case '아프리카 제비':
-      return new AfricanSwallow(bird);
-    case '노르웨이 파랑 앵무':
-      return new NorwegianBlueParrot(bird);
-    default:
-      return new Bird(bird);
-  }
+function createBird(data) {
+  return new Bird(data);
 }
 
 class Bird {
   constructor(data) {
     this._name = data.name;
     this._plumage = data.plumage;
+    this._specialPlumage = this.selectSpecialDelegate(data);
   }
 
   get name() {
@@ -22,7 +14,33 @@ class Bird {
   }
 
   get plumage() {
-    return this._plumage || '보통이다';
+    return this._specialDelegate.plumage;
+  }
+
+  get airSpeedVelocity() {
+    return this._speciesDelegate.airSpeedVelocity;
+  }
+
+  selectSpecialDelegate(data) {
+    switch (data.type) {
+      case '유럽 제비':
+        return new EuropeanSwallowDelegate(data, this);
+      case '아프리카 제비':
+        return new AfricanSwallowDelegate(data, this);
+      case '노르웨이 파랑 앵무':
+        return new NorwegianBlueParrotDelegate(data, this);
+      default:
+        return new SpeciesDelegate(data, this);
+    }
+  }
+}
+class SpeciesDelegate {
+  constructor(data, bird) {
+    this._bird = bird;
+  }
+
+  get plumage() {
+    return this._bird._plumage || '보통이다';
   }
 
   get airSpeedVelocity() {
@@ -30,15 +48,15 @@ class Bird {
   }
 }
 
-class EuropeanSwallow extends Bird {
+class EuropeanSwallowDelegate extends SpeciesDelegate {
   get airSpeedVelocity() {
     return 35;
   }
 }
 
-class AfricanSwallow extends Bird {
+class AfricanSwallowDelegate extends SpeciesDelegate {
   constructor(data) {
-    super(data);
+    super(data, bird);
 
     this._numberOfCoconuts = data.numberOfCoconuts;
   }
@@ -46,25 +64,37 @@ class AfricanSwallow extends Bird {
   get airSpeedVelocity() {
     return 40 - 2 * this._numberOfCoconuts;
   }
+
+  get plumage() {
+    return this._bird._plumage || '보통이다';
+  }
 }
 
-class NorwegianBlueParrot extends Bird {
-  constructor(data) {
-    super(data);
+class NorwegianBlueParrotDelegate extends SpeciesDelegate {
+  constructor(data, bird) {
+    super(data, bird);
 
     this._voltage = data.voltage;
     this._isNailed = data.isNailed;
+  }
+
+  get airSpeedVelocity() {
+    return this._isNailed ? 0 : 10 + this._voltage / 10;
   }
 
   get plumage() {
     if (this._voltage > 100) {
       return '그을렸다';
     } else {
-      return this._plumage || '예쁘다';
+      return this._bird_plumage || '예쁘다';
     }
   }
-
-  get airSpeedVelocity() {
-    return this._isNailed ? 0 : 10 + this._voltage / 10;
-  }
 }
+
+/**
+ * 이 예제는 좀 잘못됐다
+ * 위임보다는 상속이 더 알맞는 케이스
+ * 위임을 쓰는 경우는 외부로부터 주입을 받기 위해서 사용하는데,
+ * 이렇게 내부적으로 주어진 데이터에 의거해서 위임을 만들면 테스트하기도 어렵다
+ * delegate은 외부에서 주입받는 식으로 만들자
+ */
--- ch12/12-10.js
+++ ch12/12-10.js
@@ -17,3 +17,12 @@ class RedPrinter extends Printer {
 
 const printers = [new Printer(), new RedPrinter()];
 printers.forEach((printer) => printer.print());
+
+/**
+ * 상속의 문제점,
+ * 다중 상속이 되지 않는다
+ * RedPrinter에서 Network 클래스의 send 기능을 이용하고 싶은 경우,
+ * 이미 Printer를 상속하기 때문에 Network를 상속할 수 없다
+ * 따라서, 상속을 사용할 경우 수정과 확장이 어렵다
+ * 이럴 때 상속을 쓰지 않고 composition (delegation)을 쓸 수 있다
+ */
